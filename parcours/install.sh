7cbb948b2eacde73d81b18b0911dac608258f60a
mael <mael.minard@epita.fr>
2026-06-06 14:35:09 +0200

    meta: flake.lock, parcours install and requirements

---
+++ parcours/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+BASE_DIR="$(pwd)"
+VENV="$BASE_DIR/env"
+
+# create venv
+if [ ! -d "$VENV" ]; then
+    python3 -m venv "$VENV"
+fi
+# deps
+"$VENV/bin/pip" install -q -r "$BASE_DIR/requirements.txt"
---
+++ parcours/verdun/verdun.py
@@ -0,0 +1,3 @@
+import osmnx as ox
+
+G = ox.graph_from_place("Lyon, France", network_type="drive")
